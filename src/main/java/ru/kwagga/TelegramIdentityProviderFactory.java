1813faad62c943baadbada78231f942d37eb2863
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2025-08-09 21:15:57 +0300

    feat: Add auth data validator

--- src/main/java/ru/kwagga/TelegramIdentityProviderFactory.java
+++ src/main/java/ru/kwagga/TelegramIdentityProviderFactory.java
@@ -1,6 +1,5 @@
 package ru.kwagga;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
@@ -14,12 +13,13 @@ import ru.kwagga.config.TelegramIdentityProviderConfig;
 
 public class TelegramIdentityProviderFactory extends AbstractIdentityProviderFactory<TelegramIdentityProvider> {
 
-    private final String IDENTITY_PROVIDER_ID = "telegram";
-    private final String IDENTITY_PROVIDER_NAME = "Telegram";
-    private final String HELP_TEXT = "Authenticator that allows you to login using Telegram Web Login Widget";
+    private static final String IDENTITY_PROVIDER_ID = "telegram";
+    private static final String IDENTITY_PROVIDER_NAME = "Telegram";
+    private static final String HELP_TEXT = "Authenticator that allows you to login using Telegram Web Login Widget";
 
-    public static final String AUTH_TIME_DELTA_CONFIG_NAME = "telegram_auth_time_delta";
-    public static final String DEFAULT_AUTH_TIME_DELTA = "60";
+    private static final String AUTH_TIME_DELTA_CONFIG_NAME = "telegram_auth_time_delta";
+    private static final String DEFAULT_AUTH_TIME_DELTA = "60";
+    private static final String BOT_TOKEN = "bot_token";
 
     @Override
     public String getName() {
@@ -50,17 +50,29 @@ public class TelegramIdentityProviderFactory extends AbstractIdentityProviderFac
     public List<ProviderConfigProperty> getConfigProperties() {
         return ProviderConfigurationBuilder.create()
             .property()
-            .name(AUTH_TIME_DELTA_CONFIG_NAME)
-            .label("Auth time delta (in seconds)")
-            .helpText("""
-                The maximum delta (in seconds) between the time of successful authorization in Telegram ('auth_date' parameter) and the time of receive the authorization request in Keycloak.
-                After this time, request will be considered expired, even if it contains valid data.
-                This is necessary to avoid reuse of authorization data in case of leakage.
-                """)
-            .type(ProviderConfigProperty.STRING_TYPE)
-            .secret(false)
-            .required(true)
-            .defaultValue(DEFAULT_AUTH_TIME_DELTA)
+                .name(AUTH_TIME_DELTA_CONFIG_NAME)
+                .label("Auth time delta (in seconds)")
+                .helpText("""
+                    The maximum delta (in seconds) between the time of successful authorization in Telegram ('auth_date' parameter) and the time of receive the authorization request in Keycloak.
+                    After this time, request will be considered expired, even if it contains valid data.
+                    This is necessary to avoid reuse of authorization data in case of leakage.
+                    """)
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .secret(false)
+                .required(true)
+                .defaultValue(DEFAULT_AUTH_TIME_DELTA)
+            .add()
+            .property()
+                .name(BOT_TOKEN)
+                .label("Telegram Bot Token")
+                .helpText(
+                    """
+                    Telegram Bot Token for authorization and generating hash     
+                    """
+                )
+                .type(ProviderConfigProperty.PASSWORD)
+                .secret(true)
+                .required(true)
             .add()
             .build();
     }
---
+++ src/main/java/ru/kwagga/validator/TelegramAuthDataValidator.java
@@ -0,0 +1,5 @@
+package ru.kwagga.validator;
+
+public class TelegramAuthDataValidator {
+
+}
